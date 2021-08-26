95ee90c34d822d198fb5c0bb6f7eb28530ac51f4
Amie Hood <amiehood1@gmail.com>
2021-08-26 13:02:56 -0400

    cleaned up code

--- src/Components/PokeFetch/PokeFetch.js
+++ src/Components/PokeFetch/PokeFetch.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import './PokeFetch.css';
-// import Timer from '../Timer';
 
 
 class PokeFetch extends Component {
@@ -36,16 +35,11 @@ class PokeFetch extends Component {
 
   componentDidMount() {
     console.log('componentDidMount');
-    // this.timer = setTimeout(() => {this.state, 10000})
   }
 
-  // componentWillUnmount() {
-  //   clearInterval(this.timer)
-  //   console.log(timer);
-  // }
+ 
 
   startTimer = () => {
-    // this.setState({timeRemaining: this.state.timeRemaining -1})
     console.log(this.state);
 
     let timer = setInterval(() => {
@@ -57,33 +51,8 @@ class PokeFetch extends Component {
         clearInterval(timer)
       }
     }, 1000);
-
-
-
-    //attempt 3:
-    // this.setState({
-    //   isOn: true,
-    //   time: this.state.time,
-    //   start: Date.now() - this.state.time
-    // })
-    // this.timer = setInterval(() => this.setState({
-    //   time: Date.now() - this.state.start
-    // }), 1)
-
-    // Attempt 2:
-    // setInterval(function(){
-    // if(this.state.timeRemaining <= 0){
-    //     clearInterval();
-    // } else {
-    //     document.getElementById("progressBar").value = 10 - this.state.timeRemaining;
-    //     this.state.timeRemaining -= 1;
-    // }}, 1000);
     console.log(this.state.time);
 }  
-
-  // resetTimer = (x) => {
-  //   this.setState({time: 0, isOn: false})
-  // }
 
   render() {
     return (
