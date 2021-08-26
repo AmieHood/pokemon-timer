c52447653100aa3c2948c75b6828e0521258070a
Amie Hood <amiehood1@gmail.com>
2021-08-26 13:16:27 -0400

    added status bar and fixed shadow size

--- src/Components/PokeFetch/PokeFetch.js
+++ src/Components/PokeFetch/PokeFetch.js
@@ -37,28 +37,30 @@ class PokeFetch extends Component {
     console.log('componentDidMount');
   }
 
- 
-
   startTimer = () => {
     console.log(this.state);
+    let timeLeft = 10
 
     let timer = setInterval(() => {
       if(this.state.count > 0){
         this.setState(prevState => ({
           count: prevState.count - 1
         }))
+        document.getElementById("progressBar").value = 11 - timeLeft;
+        timeLeft -= 1;
       } else {
         clearInterval(timer)
       }
     }, 1000);
     console.log(this.state.time);
-}  
+  }  
 
   render() {
     return (
       <div className={'wrapper'}>
         <button className={'start'} onClick={() => this.fetchPokemon() && this.startTimer()}>Start!</button>
-        <h1 className={'timer'} >{this.state.count}</h1>
+        <h1 className={'timer'} ><progress value="0" max="10" id="progressBar"></progress></h1>
+        
         <div className={'pokeWrap'}>
           {this.state.count >0 ? (
           <>
